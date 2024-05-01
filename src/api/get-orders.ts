bc0d95875e6ee287c4635832d7f431b741abe218
Ian <ssianfilipe@gmail.com>
2024-05-01 19:32:30 -0300

    feat: add orders related connections

---
+++ src/api/get-orders.ts
@@ -0,0 +1,41 @@
+import { api } from '@/lib/axios'
+
+export interface GetOrdersQuery {
+  pageIndex?: number | null
+  orderId?: string | null
+  customerName?: string | null
+  status?: string | null
+}
+
+export interface GetOrdersResponse {
+  orders: {
+    status: 'pending' | 'processing' | 'delivering' | 'delivered' | 'canceled'
+    customerName: string
+    orderId: string
+    createdAt: string
+    total: number
+  }[]
+  meta: {
+    pageIndex: number
+    perPage: number
+    totalCount: number
+  }
+}
+
+export async function getOrders({
+  pageIndex,
+  orderId,
+  customerName,
+  status,
+}: GetOrdersQuery) {
+  const response = await api.get<GetOrdersResponse>('/orders', {
+    params: {
+      pageIndex,
+      orderId,
+      customerName,
+      status,
+    },
+  })
+
+  return response.data
+}
