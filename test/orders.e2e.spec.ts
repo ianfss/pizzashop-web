9e524c10f2be5f11214485b32e480bbc84ae7b9b
Ian <ssianfilipe@gmail.com>
2024-05-10 21:15:51 -0300

    test: e2e async assertions

--- test/orders.e2e.spec.ts
+++ test/orders.e2e.spec.ts
@@ -77,7 +77,5 @@ test('filter by status', async ({ page }) => {
 
   await page.getByRole('button', { name: 'Filtrar resultados' }).click()
 
-  const tableRows = await page.getByRole('cell', { name: 'Pendente' }).all()
-
-  await expect(tableRows).toHaveLength(10)
+  await expect(page.getByRole('cell', { name: 'Pendente' })).toHaveCount(10)
 })
